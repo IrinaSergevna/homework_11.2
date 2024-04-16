cdd86101dad0b1a73869d351725b1635d7fdeb5e
isa <ir.saldaeva@gmail.com>
2024-04-16 13:17:58 +0300

    добавила описание к функциям

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def str_upper(text):
+    """
+    возвращает заглавными буквами строку
+    """
     return text.upper()
+
+def str_title(text):
+    """
+    возвращает заглавными первые буквы каждого слова в строке
+    """
+    return text.title()
